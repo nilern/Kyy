cce8d120329eb5ee28e80ddbc163a4e0d3da5d2b
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2020-11-09 20:10:26 +0200

    Add echoing REPL.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+const PROMPT: &'static str = ">>> ";
+
 fn main() {
-    println!("Hello, world!");
+    let mut repl = rustyline::Editor::<()>::new();
+
+    loop {
+        match repl.readline(PROMPT) {
+            Ok(line) => println!("{}", line),
+            Err(err) => {
+                println!("Readline error: {:?}", err);
+                break;
+            }
+        }
+    }
 }
+
